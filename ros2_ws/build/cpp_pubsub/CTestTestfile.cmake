9255750bf6a247b959700f0766a92eb52fce5173
Metal-Angel <508768343@qq.com>
2025-12-12 18:02:39 +0800

    Ubuntu

---
+++ ros2_ws/build/cpp_pubsub/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/metal_angel/Desktop/Code/ly/ros2_ws/cpp_pubsub
+# Build directory: /home/metal_angel/Desktop/Code/ly/ros2_ws/build/cpp_pubsub
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
